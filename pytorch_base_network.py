849027987943cbfa5dc585bb89221a831dd0c6cf
Gianni Casagrande <gianni.casagrande.mail@gmail.com>
2019-01-18 00:28:55 +0100

    feat: added model save and load functionalities
    added functionality and tested visually on a simple regression example

--- pytorch_base_network.py
+++ pytorch_base_network.py
@@ -1,7 +1,9 @@
-from torch import nn
-from torch import optim
-import torch.nn.functional as F
+from collections import namedtuple
+
 import torch
+import torch.nn.functional as tnn_functional
+from torch import nn
+
 
 def is_iterable(element):
     try:
@@ -15,8 +17,8 @@ def is_iterable(element):
 def squeeze_iterable(element):
     """if input is an iterable with a single element, return its value. """
     in_iterable = is_iterable(element)
-    out_value= element
-    out_iterable=in_iterable
+    out_value = element
+    out_iterable = in_iterable
     if in_iterable:
         if len(element) == 1:
             # this is the case in which i have a single element iterable.
@@ -25,28 +27,54 @@ def squeeze_iterable(element):
     return [out_value, out_iterable]
 
 
-
 class PyTorchBaseNetwork(nn.Module):
     """
     Basic PyTorch Network composed of Convolutional and linear layers.
     The Convolutional linear layers are positioned before the linear layer by default. The network only supports linear
     layers for the moment.
     """
-    def __init__(self, input_shape = (784,), conv_layers = None, lin_layers = [128,64], output_shape=(10,)):
+
+    version = (0, 5)
+
+    def __init__(self, input_shape=(784,), conv_layers=None, lin_layers=(128, 64), output_shape=(10,), dropout_p=0):
         """ Network architecture initialization according to linear and convolutional layers features """
         super().__init__()
-        [input_shape, iterable_input] = squeeze_iterable(input_shape)
-        [output_shape, dump] = squeeze_iterable(output_shape)
-        self.all_linear_network = not iterable_input
 
+        if isinstance(input_shape, str):
+            ckp=input_shape
+            saved = torch.load(ckp)
+            if saved["version"] != self.version:
+                raise ImportError(
+                    "PyTorchBaseNetwork is now at version " + self.version + " but model was saved at version " + saved[
+                        'version'])
+            print("loading network " + saved["description"])
+
+            self.initialise(input_shape=saved["input_shape"], conv_layers=saved["conv_layers"],
+                            lin_layers=saved["lin_layers"], output_shape=saved["output_shape"],
+                            dropout_p=saved["dropout_p"])
+            self.load_state_dict(saved["state_dict"])
+        else:
+            self.initialise(input_shape=input_shape, conv_layers=conv_layers,
+                            lin_layers=lin_layers, output_shape=output_shape, dropout_p=dropout_p)
+
+    def initialise(self, input_shape=(784,), conv_layers=None, lin_layers=(128, 64), output_shape=(10,), dropout_p=0):
+        self.input_shape=input_shape
+        self.conv_layers=conv_layers
+        self.lin_layers=lin_layers
+        self.output_shape=output_shape
+        self.dropout_p=dropout_p
+        [input_shape, iterable_input] = squeeze_iterable(input_shape)
+        [output_shape, _] = squeeze_iterable(output_shape)
+        self.all_linear_network = not iterable_input
+        self.dropout_p = dropout_p
         if self.all_linear_network:
             assert (conv_layers is None)
             prev_layer_n = input_shape
-            self.fc_layers=nn.ModuleList()
+            self.fc_layers = nn.ModuleList()
             for curr_layer_n in lin_layers:
                 self.fc_layers.append(nn.Linear(prev_layer_n, curr_layer_n))
                 prev_layer_n = curr_layer_n
-            self.out_layer = nn.Linear(prev_layer_n,output_shape)
+            self.out_layer = nn.Linear(prev_layer_n, output_shape)
         else:
             assert (conv_layers is not None)
             print("Convolutional Layers not supported for the moment")
@@ -64,14 +92,20 @@ class PyTorchBaseNetwork(nn.Module):
 
         for fc_layer in self.fc_layers:
             x = fc_layer(x)
-            x = F.relu(x)
+            x = tnn_functional.relu(x)
+            if self.dropout_p > 0:
+                x = tnn_functional.dropout(x, p=self.dropout_p)
         x = self.out_layer(x)
-        #x = F.softmax(x, dim=1)
         return x
 
 
 
+    def save_model(self, checkpoint_file, description=None):
 
+        tosave = {"version": self.version, "input_shape": self.input_shape, "conv_layers": self.conv_layers,
+                  "lin_layers": self.lin_layers, "output_shape": self.output_shape, "dropout_p": self.dropout_p,
+                  "state_dict": self.state_dict(), "description": description}
+        torch.save(tosave, checkpoint_file)
 
 
 
--- test_pytorch_base_network.py
+++ test_pytorch_base_network.py
@@ -1,5 +1,4 @@
 import unittest
-
 import numpy as np
 import torch
 
@@ -7,7 +6,7 @@ from pytorch_base_network import PyTorchBaseNetwork
 
 
 class TestPyTorchBaseNetwork(unittest.TestCase):
-
+    # verify the types that forward accepts
     def test_single_tensor(self):
         nn = PyTorchBaseNetwork(input_shape=(5,), lin_layers=[100, 100], output_shape=(4,))
         ten = torch.tensor([1, 2, 3, 4, 5]).float()
@@ -54,6 +53,38 @@ class TestPyTorchBaseNetwork(unittest.TestCase):
         out = nn.forward(ten)
         self.assertTrue(isinstance(nn.forward(ten), torch.FloatTensor))
         self.assertTupleEqual(out.shape, (2, 3, 4))
+
+    #verify save and load behavior
+    def test_loadsave_model(self):
+        nn = PyTorchBaseNetwork(input_shape=(2,), lin_layers=[4, 4], output_shape=(2,), dropout_p=0.1)
+        nn.train()
+        true_fun = lambda x : torch.tensor(np.abs(np.matmul(x.numpy(), np.array(([1, 2], [-3, 1])))))
+        optimizer = torch.optim.Adam(nn.parameters())
+        for i in range(10000):
+            batch=torch.randn(100, 2)
+            target = true_fun(batch)
+            loss=torch.nn.MSELoss()(target.float(), nn.forward(batch))
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if i%1000==0:
+                print(nn.state_dict())
+
+        nn.eval()
+        test_batch=torch.randn(10, 2)
+        print(true_fun(test_batch))
+        print(nn.forward(test_batch))
+
+        nn.save_model("saved.ckp", "trivial network")
+
+        nn2 = PyTorchBaseNetwork("saved.ckp")
+        test_batch=torch.randn(10, 2)
+        print(true_fun(test_batch))
+        print(nn.forward(test_batch))
+
+
+
+
 
 
 if __name__ == '__main__':
